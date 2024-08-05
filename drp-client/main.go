94e59db60e42c57732e357840f4784f2ba0d4599
IyNew <wtang3@nd.edu>
2024-08-05 05:41:39 +0000

    small modifications to the client structure

--- drp-client/main.go
+++ drp-client/main.go
@@ -5,56 +5,85 @@ import (
 	"net/http"
 	// "drp-client/handlers"
 	"encoding/json"
+	"bytes"
+	"context"
+	"crypto/x509"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"time"
+
+	"github.com/IyNew/FabricBackend/drp-client/utils"
 
 	"github.com/gorilla/mux"
+	"github.com/hyperledger/fabric-gateway/pkg/client"
+	"github.com/hyperledger/fabric-gateway/pkg/identity"
+	"github.com/hyperledger/fabric-protos-go-apiv2/gateway"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
 )
 
-type User struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-}
+const (
+	mspID        = "Org1MSP"
+	cryptoPath   = "../fabric-samples/test-network/organizations/peerOrganizations/org1.example.com"
+	certPath     = cryptoPath + "/users/User1@org1.example.com/msp/signcerts"
+	keyPath      = cryptoPath + "/users/User1@org1.example.com/msp/keystore"
+	tlsCertPath  = cryptoPath + "/peers/peer0.org1.example.com/tls/ca.crt"
+	peerEndpoint = "dns:///localhost:7051"
+	gatewayPeer  = "peer0.org1.example.com"
+)
 
-var users = []User{
-	{ID: "1", Name: "John Doe"},
-	{ID: "2", Name: "Jane Doe"},
-}
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hello, world!"))
-}
+// type User struct {
+// 	ID   string `json:"id"`
+// 	Name string `json:"name"`
+// }
 
-func GetUsers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(users)
-}
+// var users = []User{
+// 	{ID: "1", Name: "John Doe"},
+// 	{ID: "2", Name: "Jane Doe"},
+// }
 
-func GetUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for _, user := range users {
-		if user.ID == params["id"] {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(user)
-			return
-		}
-	}
-	w.WriteHeader(http.StatusNotFound)
-	w.Write([]byte("User not found"))
-}
+// func HelloHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write([]byte("Hello, world!"))
+// }
 
-func CreateUser(w http.ResponseWriter, r *http.Request) {
-	var newUser User
-	if err := json.NewDecoder(r.Body).Decode(&newUser); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	users = append(users, newUser)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(newUser)
-}
+// func GetUsers(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	json.NewEncoder(w).Encode(users)
+// }
+
+// func GetUser(w http.ResponseWriter, r *http.Request) {
+// 	params := mux.Vars(r)
+// 	for _, user := range users {
+// 		if user.ID == params["id"] {
+// 			w.Header().Set("Content-Type", "application/json")
+// 			w.WriteHeader(http.StatusOK)
+// 			json.NewEncoder(w).Encode(user)
+// 			return
+// 		}
+// 	}
+// 	w.WriteHeader(http.StatusNotFound)
+// 	w.Write([]byte("User not found"))
+// }
+
+// func CreateUser(w http.ResponseWriter, r *http.Request) {
+// 	var newUser User
+// 	if err := json.NewDecoder(r.Body).Decode(&newUser); err != nil {
+// 		w.WriteHeader(http.StatusBadRequest)
+// 		return
+// 	}
+// 	users = append(users, newUser)
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.WriteHeader(http.StatusCreated)
+// 	json.NewEncoder(w).Encode(newUser)
+// }
 
 
 func main() {
